34f1adb2b4e091ec408503d2cefdb24243e9e307
lax1089 <alexanderkpage@gmail.com>
2019-03-13 16:07:34 -0400

    changing from jar to war packaging

--- src/main/java/io/demo/bank/DigitalBankApplication.java
+++ src/main/java/io/demo/bank/DigitalBankApplication.java
@@ -2,10 +2,17 @@ package io.demo.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DigitalBankApplication {
+public class DigitalBankApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DigitalBankApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalBankApplication.class, args);
 	}
