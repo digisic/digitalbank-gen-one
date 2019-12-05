8372bfe75e2eaa41b77a4e72b1204bded975bb44
ma601572 <ma601572@192.168.0.31>
2019-12-05 16:28:58 -0600

    Changed Correlation Scheme to CorreliationId only

--- src/main/java/io/demo/bank/jms/CreditAppProducer.java
+++ src/main/java/io/demo/bank/jms/CreditAppProducer.java
@@ -1,20 +1,14 @@
 package io.demo.bank.jms;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.demo.bank.model.CreditApplication;
 
 
@@ -31,29 +25,23 @@ public class CreditAppProducer {
 
     public String sendCreditApplication(CreditApplication app){
     	
+    	String correlationId = UUID.randomUUID().toString();
     	ObjectMapper mapper = new ObjectMapper();
-    	final AtomicReference<Message> message = new AtomicReference<>();
     	
     	try {
     		
 			String jsonMessage = mapper.writeValueAsString(app);
 			
-			jmsTemplate.convertAndSend(destinationQueue, jsonMessage, messagePostProcessor -> {
-			      message.set(messagePostProcessor);
-			      return messagePostProcessor;
+			jmsTemplate.convertAndSend(destinationQueue, jsonMessage, m -> {
+				m.setJMSCorrelationID(correlationId);  
+			    return m;
 			});
-
-			/*
-			 * Note the message Id of the sent message is copied to the correlation Id on the
-			 * related responding message
-			 */
-			String messageId = message.get().getJMSMessageID();
 			    
-			LOG.debug("Send Message Correlation Id: " + messageId);
+			LOG.debug("Send Message Correlation Id: " + correlationId);
 
-			return messageId;
+			return correlationId;
 			
-		} catch (JsonProcessingException | JMSException e) {
+		} catch (JsonProcessingException e) {
 			
 			LOG.error("Error: Could not send application to credit provider.");
 			e.printStackTrace();
