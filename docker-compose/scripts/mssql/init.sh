5b401b329ffa1b86531c9d7a8a900b4d209bae0b
Mark Asbury <asburymr@yahoo.com>
2020-04-09 11:11:34 -0500

    Improved SQL Server deployment

--- docker-compose/scripts/mssql/init.sh
+++ docker-compose/scripts/mssql/init.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-# wait for SQL Server to come up
-echo Initializing Databases will start in 30 seconds...
-sleep 30
-echo Initializing Databases
+echo ==========================================================================================
+echo ==========================================================================================
+echo ====                                                                                  ====
+echo ==== Digital Bank MSSQL Server Database Setup                                         ====
+echo ====                                                                                  ====
+echo ==== Initializing Application Database and User will start in 90 seconds...           ====
+echo ====                                                                                  ====
+echo ==========================================================================================
+echo ==========================================================================================
 
-# run the init.sql script to create the databases
+# wait for SQL Server to come up before create any new objects
+
+sleep 90
+
+echo ==========================================================================================
+echo ==========================================================================================
+echo ====                                                                                  ====
+echo ==== Initializing Application Database and User                                       ====
+echo ====                                                                                  ====
+
+# run the init.sql script to create the databases and user
+
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P Demo123! -i /usr/src/init.sql
 
-
-
+echo ====                                                                                  ====
+echo ====                                                                                  ====
+echo ==== Initialization Complete!                                                         ====
+echo ====                                                                                  ====
+echo ==========================================================================================
+echo ==========================================================================================
